df8ba77a1d6d643d9d1c165abfbc845631121335
shay-j <sy020288@gmail.com>
2024-12-09 16:25:24 +0200

    uploaded

--- Hello.java
+++ Hello.java
@@ -1,7 +1,7 @@
 public class Hello {
 	public static void main(String[] args) {
 		for(int i=1;i<=10;i++) {
-			System.out.println("test finished ..."+i);
+			System.out.println("test finished..."+i);
 		}
 	}
 }
